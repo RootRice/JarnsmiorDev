42c1f939ebd1def670b0963c680f18cb49821d40
Teodor G <grigor.teodor@gmail.com>
2020-06-10 21:41:50 +0200

    added table object
    -created table tag
    -added on click function

--- Assets/Scripts/MainCharacterSmithy.cs
+++ Assets/Scripts/MainCharacterSmithy.cs
@@ -15,7 +15,7 @@ public class MainCharacterSmithy : MonoBehaviour
 
     bool canControl = false;
 
-    GameObject anvilSmith;
+    GameObject table;
     GameObject furnaceSmith;
     GameObject bookSmith;
     GameObject elongateUI;
@@ -28,7 +28,7 @@ public class MainCharacterSmithy : MonoBehaviour
         isMoving = true;
         myAnimator = GetComponent<Animator>();
         SetTarget(new Vector3(11.48f, 10.02f, 0f), new Vector3(16.18f, 5.04f, 0f), new Vector3(19.68f, 5.04f, 0f));
-        anvilSmith = GameObject.FindGameObjectWithTag("MCAnvil");
+        table = GameObject.FindGameObjectWithTag("SF_Table");
         furnaceSmith = GameObject.FindGameObjectWithTag("MCFurnace");
         bookSmith = GameObject.FindGameObjectWithTag("MCBook");
         bookSmith.SetActive(false);
