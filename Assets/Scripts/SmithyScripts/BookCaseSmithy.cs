eda500cf4c086d3e280ec426b95b8ea2601f3990
RootRice <michaelhindle95@gmail.com>
2020-06-10 22:29:47 +0100

    Implemented particle effects and final background

---
+++ Assets/Scripts/SmithyScripts/BookCaseSmithy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookCaseSmithy : MonoBehaviour {
+
+    MainCharacterSmithy mainCharacterScript;
+    CameraScript cameraScript;
+
+    void Start()
+    {
+
+        GameObject mainCharacter = GameObject.FindGameObjectWithTag("MainCharacterSmithy");
+        mainCharacterScript = (MainCharacterSmithy)mainCharacter.GetComponent(typeof(MainCharacterSmithy));
+
+        GameObject cameraObj = GameObject.FindGameObjectWithTag("MainCamera");
+        cameraScript = (CameraScript)cameraObj.GetComponent(typeof(CameraScript));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        if (mainCharacterScript != null && mainCharacterScript.GetControl())
+        {
+            mainCharacterScript.SetTarget(new Vector3(3.34f, 5.04f, 0));
+            cameraScript.SetTarget(new Vector3(2.56f, 4.74f, -10f), 0.68f);
+        }
+    }
+}
---
+++ Assets/Scripts/SmithyScripts/MCBookSmithy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCBookSmithy : MonoBehaviour {
+
+    Animator myAnimator;
+    CameraScript cameraScript;
+    // Use this for initialization
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        GameObject cameraObj = GameObject.FindGameObjectWithTag("MainCamera");
+        cameraScript = (CameraScript)cameraObj.GetComponent(typeof(CameraScript));
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (myAnimator.GetCurrentAnimatorStateInfo(0).IsName("BookIdle"))
+        {
+            cameraScript.SetTarget(new Vector3(52f, 18.39f, -10f), 6.07f);
+            cameraScript.SetPosition(new Vector3(52f, 18.39f, -10f), 6.07f);
+            gameObject.SetActive(false);
+        }
+    }
+}
