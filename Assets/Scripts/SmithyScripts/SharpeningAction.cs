d49e9ea3f2aa8f2ff63ed3bc1919ffd1c2690002
Teodor G <grigor.teodor@gmail.com>
2020-06-25 17:40:01 +0200

    added files for the new version

---
+++ Assets/Scripts/SmithyScripts/SharpeningAction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharpeningAction : MonoBehaviour {
+
+    GameObject swordHolder;
+    SharpeningAlghoritm mSharpeningAlghoritm;
+
+	// Use this for initialization
+	void Start () {
+        swordHolder = GameObject.FindGameObjectWithTag("SwordHolder");
+        mSharpeningAlghoritm = (SharpeningAlghoritm)swordHolder.GetComponent(typeof(SharpeningAlghoritm));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Restart()
+	{
+        swordHolder = GameObject.FindGameObjectWithTag("SwordHolder");
+        mSharpeningAlghoritm = (SharpeningAlghoritm)swordHolder.GetComponent(typeof(SharpeningAlghoritm));
+		mSharpeningAlghoritm.Restart();
+	}
+
+	public void Stop()
+	{
+		mSharpeningAlghoritm.Stop();
+	}
+
+}
